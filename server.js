2d47215e1e44a8a614451f55a9fae1de3e8e21f0
Chris Perko <cperko@teachers-teachers.com>
2017-02-12 18:51:33 -0600

    Setup mongoose and todo schema

--- server.js
+++ server.js
@@ -3,9 +3,12 @@
 var express     = require('express');
 var app         = express();
 var bodyParser  = require('body-parser');
+var mongoose    = require('mongoose');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://reactuser:password@ds151059.mlab.com:51059/react_todo');
 
 var port = process.env.PORT || 8080;
 
