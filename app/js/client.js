1173dfb50760bb34bb95ad04e9b0c76c9b454e61
Chris Perko <cperko@teachers-teachers.com>
2017-02-13 18:05:54 -0600

    Setup basic components

--- app/js/client.js
+++ app/js/client.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Layout extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.message = 'Hello, world!';
-    }
-
-    render() {
-        return (
-            <h1>{this.message}</h1>
-        );
-    }
-}
+import Layout from './components/Layout';
 
 const app = document.getElementById('app');
 
