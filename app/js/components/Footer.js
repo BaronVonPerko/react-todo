3a679e58f8bec34537a514aec53bef1d4353de18
cperko <cperko@teachers-teachers.com>
2017-02-18 12:47:42 -0600

    Styling for footer

--- app/js/components/Footer.js
+++ app/js/components/Footer.js
@@ -4,7 +4,13 @@ import React from 'react';
 export default class Footer extends React.Component {
     render() {
         return (
-            <footer>This is the footer</footer>
+            <footer>
+                <span>&copy; Chris Perko 2017</span>
+                <span>|</span> 
+                <span>
+                    <a href="http://www.chrisperko.net/">ChrisPerko.NET</a>
+                </span>
+            </footer>
         );
     }
 }
