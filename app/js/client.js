cf5fd3e16d7b1d1838fc5a39c7b7fbf19a8cba83
cperko <cperko@teachers-teachers.com>
2017-02-18 12:44:26 -0600

    Initial setup for styling with SASS

--- app/js/client.js
+++ app/js/client.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import Main from './components/Main';
 
+require('./../style/main.scss');
+
 const app = document.getElementById('app');
 
 ReactDOM.render(<Main />, app);
